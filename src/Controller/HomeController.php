0801a8f9095119d295cff9304b6ae83ee4e13d22
Damien JANDARD <damien.jandard@gmail.com>
2023-06-23 15:56:46 +0200

    Creation of the homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findAll();
+        return $this->render('home/home.html.twig', compact('tricks'));
+    }
+}
